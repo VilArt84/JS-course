7109ae5cac9af291a0b7ba2d1ffecda5f918c32f
VilArt <vilart.kbr@gmail.com>
2019-12-09 15:45:04 +0300

    Это на первый комит

---
+++ lessons 1/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+
+// var leftBorderWidth = 1;
+// alert("Hello world");
+// let answer = confirm("Are you here?");
+// console.log(answer);
+
+// let answer = prompt("Есть ли вам 18?", "Да");
+// console.log(typeof(answer));
+
+let incr = 10,
+    decr= 10;
+
+
+console.log(incr);
+console.log(decr);
