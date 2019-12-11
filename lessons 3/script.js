61bb2782399cde7e801ce987c148c7054058b365
VilArt <vilart.kbr@gmail.com>
2019-12-11 16:29:00 +0300

    Четвертый урок

---
+++ lessons 3/script.js
@@ -0,0 +1,35 @@
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     alert(text);
+//     let num = 10;
+//     console.log(num);
+// }
+
+// showFirstMessage("Hello World!");
+// console.log(num);
+
+// function calc(a,b) {
+//     return (a+b);
+// }
+
+let calc = (a,b) => a+b;
+console.log(calc(3,4));
+console.log(calc(8,4));
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+let anotherNum = retVar();
+console.log(anotherNum);
+
+let str = "text";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+
+let twelve = "12.2px";
+// console.log(Math.round(twelve));
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));
